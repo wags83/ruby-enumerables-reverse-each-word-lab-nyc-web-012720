9045d433a8f0c6d9b62258fdcca6e5c0d68110fc
Greg Wagner <gpwagner@gmail.com>
2020-01-16 16:08:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,16 @@
 def reverse_each_word(string)
   array = []
   array = string.split
-  array.collect {|v| v.reverse}
+  i=0
+  while i < array.length do
+  reverse_element = array[i].reverse
+  reverse_string += "#{reverse_element} "
 end
+
+#def reverse_each_word(string)
+#  array = []
+#  array = string.split
+#  i=0
+#  while i < array.length
+#  array.collect {|v| v.reverse}
+#end
